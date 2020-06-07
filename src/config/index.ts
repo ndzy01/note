9b9aac4467f367c193812922bba3f18a90850f1a
张一 <2326962137@qq.com>
2020-06-07 11:52:53 +0800

    2020 0607 1152

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import routers from './routes'
 const config = {
   deafultUrl:{
     url: 'http://localhost:8888', // 服务地址
-    urlOnLine: 'http://www.ndzy01.com:9001', // 服务地址
+    urlOnLine: 'http://www.ndzy01.com:8888', // 服务地址
   },
   routers
 }
